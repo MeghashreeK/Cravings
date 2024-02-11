1d05ddc5f4293dc27fad847c5b83815b8133fd8c
MeghashreeK <115255491+MeghashreeK@users.noreply.github.com>
2024-02-11 12:11:39 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,33 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState,useEffect } from "react";
-// import resList from "../utils/mockData";
-import resList1 from "../utils/fasosmockData";
+import resList from "../utils/mockData";
+
 
 const Body = () => {
 
-   const [listOfRestaurant,setlistOfRestaurant]=useState(resList1);
+   const [listOfRestaurant,setlistOfRestaurant]=useState(resList);
    useEffect(()=>{fetchData()},[])
    
    const fetchData=async()=>{
-    // const data=await fetch("https://corsproxy.org/?https://www.swiggy.com/mapi/homepage/getCards?lat=13.0687993&lng=74.99360039999999");
-    const data=await fetch("https://corsproxy.org/?https://www.eatsure.com/v1/api/get_all_products/brand_id/20/store_id/10369/source_id/3");
+    const data=await fetch("https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D23.022505%26lng%3D72.5713621%26page_type%3DDESKTOP_WEB_LISTING");
     const json=await data.json();
     console.log(json);
-    setlistOfRestaurant(json.data.collections[0].products);
+    setlistOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
    }; 
 
     return (
         <div className="body">
             <button className="filter-btn" onClick={
                 () => {
-                    // let filteredList = listOfRestaurant.filter((res) => res.info.avgRating > 4);
-                    let filteredList = listOfRestaurant.filter((res) => res.rating > 4.3);
+                    let filteredList = listOfRestaurant.filter((res) => res.info.avgRating > 4);
                     setlistOfRestaurant(filteredList);
                 }}>Top Rated Restaurants</button>
             <div className="restaurantContainer">
-                {/* {listOfRestaurant.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)} */}
-                {listOfRestaurant.map(restaurant => <RestaurantCard key={restaurant.product_id} resData={restaurant} />)}
-            </div>
+                {listOfRestaurant.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)}</div>
         </div>
     );
 };
