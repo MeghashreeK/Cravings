430ec16d08fec6246f61ed7210850d816776d805
MeghashreeK <kundermeghashree279@gmail.com>
2024-02-25 14:48:30 +0530

    Added accordian

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,15 +1,24 @@
 import ItemList from "./ItemList";
+import { useState } from "react";
 
-const RestaurantCategory=({data})=>{
+const RestaurantCategory=({data,showItems,setShowIndex})=>{
 //console.log(data);
+
+// const [showItems,setShowItems]=useState(false);
+handleClick=()=>{
+    setShowIndex();
+}
+
+
     return( 
-        
+        <div>
         <div className="m-10 border-2">
-            <div className="flex justify-between py-5">
+            <div className="flex justify-between py-5" onClick={handleClick}>
             <span className="px-3">{data.title} ({data.itemCards.length})</span>
             <span className="px-3">⬇️</span>
             </div>
-            <ItemList items={data.itemCards}/>
+           {showItems && <ItemList items={data.itemCards}/>}
+        </div>
         </div>
        
            
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,32 +3,38 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../customHooks.js/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
+
 const RestaurantMenu = () => {
+
+    const [showIndex,setShowIndex]=useState(0);
     const { resId } = useParams();
 
     const resInfo=useRestaurantMenu(resId);
     if (!resInfo) return <Shimmer />;
-    // console.log(resInfo);
-
+   
     const {
         name,
         avgRating,
         costForTwoMessage
     } = resInfo?.cards[2]?.card?.card?.info || {};
 
-    // const { itemCards } = resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card ||resInfo?.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
-    // const ss=resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
-    // console.log(ss);
+
     const categories=resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>c.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
-    //console.log(categories);
     return (
         <div className="text-center">
             <h2>{name}</h2>
             <h2>{avgRating}</h2>
-            {/* {itemCards && itemCards.length>0 && itemCards.map(items =><li key={items.card.info.id}>{items.card.info.name}</li>)} */}
-            {/* {categories.map((category)=><RestaurantCategory data={category?.card?.card}/>)} */}
-            {/* {categories.map((category)=><RestaurantCategory data={category?.card?.card}/>)} */}
-            {categories.map((category)=><RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card}/>)}
+            {categories.map((category,index)=><RestaurantCategory 
+            key={category?.card?.card?.title} 
+            showItems={index===showIndex && true} 
+            setShowIndex={()=>{
+                if(index!==showIndex)
+                {setShowIndex(index)}
+                else
+                {setShowIndex(null)}
+            }}
+            data={category?.card?.card}/>)}
+           
         </div>
     );
    
