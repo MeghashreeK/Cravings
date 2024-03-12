9177dc3fef227627d1d8a6b175e6fd870e2ba950
MeghashreeK <kundermeghashree279@gmail.com>
2024-03-12 20:46:13 +0530

    faq edit

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,28 @@
-import GetUserData from "./GetUserData";
 import linkedin from "../images/linkedin-logo.png";
 import github from "../images/github-logo.png";
 import { Link } from "react-router-dom";
+import resimage from "../images/restaurantfood.jpg";
 const About = () => {
     return (
         <div className="flex flex-col min-h-screen">
 
-            <div className="flex-grow">
-                <h1>About</h1>
-                <GetUserData />
-                </div>
-              
-                <div className="flex bg-black h-32 justify-center items-center sm:h-16">
-                    <div className="flex gap-5 sm:gap-8">
-                        <Link to="https://www.linkedin.com/in/meghashree-kunder-017166288/"><img className="w-8 h-8" src={linkedin} /></Link>
-                        <Link to="https://github.com/MeghashreeK"><img className="w-8 h-8" src={github} /></Link>
+            <div className="flex flex-col flex-grow px-5 justify-center">
+                <div className="flex gap-8">
+                    <img className="w-5/12 border-8 border-orange-500 rounded-[50%]" src={resimage} alt="food image" />
+                    <div className="flex flex-col gap-5">
+                    <h1 className="text-6xl font-Dancing font-bold text-orange-500">Anytime, Anywhere..</h1>
+                    <h1 className=" font-Montserrat">Welcome to Cravings, where our journey began with a passion for revolutionizing the way people experience food. Established in [Year], we set out with a clear mission: to bring convenience, quality, and satisfaction to every doorstep. Our commitment to excellence is reflected in our stringent quality and safety standards, ensuring that every meal delivered meets the highest expectations. At the heart of our service is a customer-centric approach, where we prioritize your needs, preferences, and convenience in every order. With a diverse range of restaurant partners, we offer a culinary journey that celebrates the flavors of local and international cuisines. Powered by cutting-edge technology and innovation, our platform provides seamless ordering experiences and personalized recommendations. Beyond business, we are dedicated to social responsibility, actively supporting local communities and promoting sustainable practices. Our team is composed of industry experts and passionate individuals who drive our vision forward with creativity and dedication. But don't just take our word for it—hear from our satisfied customers and restaurant partners who have experienced the [Food Delivery App Name] difference firsthand. We invite you to join us on this culinary adventure and discover why we're more than just a food delivery app—we're a partner in creating memorable dining experiences. Contact us today to learn more about our services or to share your feedback—we're always here to serve you.</h1>
                     </div>
                 </div>
-            
+            </div>
+
+            <div className="flex bg-black h-32 justify-center items-center sm:h-16">
+                <div className="flex gap-5 sm:gap-8">
+                    <Link to="https://www.linkedin.com/in/meghashree-kunder-017166288/"><img className="w-8 h-8" src={linkedin} /></Link>
+                    <Link to="https://github.com/MeghashreeK"><img className="w-8 h-8" src={github} /></Link>
+                </div>
+            </div>
+
 
         </div>
 
--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,186 +1,7 @@
-// import linkedin from "../images/linkedin-logo.png";
-// import github from "../images/github-logo.png";
-// import { Link } from "react-router-dom";
-// import downarrow from "../images/down-arrow.png";
-// import { useState } from "react";
-// import uparrow from "../images/up-arrow.png";
-
-// const FAQ = () => {
-
-//     const [answer, setAnswer] = useState(false);
-//     const showAnswer = () => {
-//         setAnswer(!answer);
-//     }
-
-//     return (
-//         <div className="flex flex-col min-h-screen">
-//             <div className="flex flex-col flex-grow gap-5 p-5">
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col border-b-2 border-b-gray-200 px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//                 <div className="flex flex-col px-3 py-6" onClick={() => showAnswer()}>
-
-//                     <div className="flex justify-between w-full">
-//                         <h1>What is Swiggy Customer Care Number?</h1>
-//                         {answer === false ? <img src={downarrow} alt="downarrow" /> : <img src={uparrow} alt="uparrow" />}
-//                     </div>
-
-//                     <div className="flex py-1">
-//                         {answer && (<p>We value our customer’s time and hence moved away from a single customer care number to a comprehensive chat-based support system for quick and easy resolution. You no longer have to go through the maze of an IVRS call support. Just search for your issue in the help section on this page and initiate a chat with us. A customer care executive will be assigned to you shortly. You can also email us your issue on support@swiggy.in
-
-//                             Note: We value your privacy and your information is safe with us. Please do not reveal any personal information, bank account number, OTP etc. to another person. A Swiggy representative will never ask you for these details. Please do not reveal these details to fraudsters and imposters claiming to be calling on our behalf. Be vigilant and do not entertain phishing calls or emails.</p>)}
-//                     </div>
-
-//                 </div>
-//             </div>
-
-//             <div className="flex bg-black h-32 justify-center items-center sm:h-16">
-//                 <div className="flex gap-5 sm:gap-8">
-//                     <Link to="https://www.linkedin.com/in/meghashree-kunder-017166288/"><img className="w-8 h-8" src={linkedin} /></Link>
-//                     <Link to="https://github.com/MeghashreeK"><img className="w-8 h-8" src={github} /></Link>
-//                 </div>
-//             </div>
-
-//         </div>
-//     );
-// }
-
-
-// export default FAQ;
-
-
-
-import linkedin from "../images/linkedin-logo.png";
-import github from "../images/github-logo.png";
-import { Link } from "react-router-dom";
 import downarrow from "../images/down-arrow.png";
 import { useEffect, useState } from "react";
 import uparrow from "../images/up-arrow.png";
 const FAQ = () => {
-
     const [answer, setAnswer] = useState(false);
     const [indexOfBox, setIndexOfBox] = useState([0]);
 
@@ -198,35 +19,25 @@ const FAQ = () => {
         const json = await data.json();
         console.log(json);
         setFaqData(json.data.issues.data);
-
     }
 
     return (
-        <div className="flex flex-col min-h-screen">
-            <div className="flex flex-col flex-grow gap-5 p-5">
-                {faqData.slice(3,-1).map((data, index) => <div key={data.id} className={`flex flex-col ${index!==faqData.length - 4 ? 'border-b-2 border-b-gray-200' : ''} px-3 py-6`} onClick={() => showAnswer(index)}>
+        <div className="flex flex-col p-5 h-screen">
+            <div className="flex flex-col gap-5 ">
+                {faqData.slice(3,-12).map((data, index) => <div key={data.id} className={`flex flex-col ${index!==faqData.length - 16 ? 'border-b-2 border-b-gray-200' : ''} px-3 py-6`} onClick={() => showAnswer(index)}>
 
                     <div className="flex justify-between w-full font-Montserrat font-medium">
-                        <h1>{data.title}</h1>
-                        {indexOfBox==index && answer === true ? <img src={uparrow} alt="uparrow" /> : <img src={downarrow} alt="downarrow" />}
+                        <h1>{data.title.replace(/Swiggy/gi, 'Cravings')}</h1>
+                        {indexOfBox==index && answer === true ? <img className="h-4" src={uparrow} alt="uparrow" /> : <img className="h-4" src={downarrow} alt="downarrow" />}
                     </div>
                     <div className="flex py-1 text-gray-400">
                         {
-                           indexOfBox==index && answer && <p>{data.description}</p>
+                           indexOfBox==index && answer && <p>{data.description.replace(/Swiggy/gi, 'Cravings')}</p>
                         }
                     </div>
 
                 </div>)}
-
             </div>
-
-            <div className="flex bg-black h-32 justify-center items-center sm:h-16">
-                <div className="flex gap-5 sm:gap-8">
-                    <Link to="https://www.linkedin.com/in/meghashree-kunder-017166288/"><img className="w-8 h-8" src={linkedin} /></Link>
-                    <Link to="https://github.com/MeghashreeK"><img className="w-8 h-8" src={github} /></Link>
-                </div>
-            </div>
-
         </div>
     );
 }
