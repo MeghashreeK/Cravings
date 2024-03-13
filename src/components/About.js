a896b4d9edfae1616cadf8fa9de78116ba804cb8
MeghashreeK <kundermeghashree279@gmail.com>
2024-03-13 10:08:10 +0530

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,28 @@ import github from "../images/github-logo.png";
 import { Link } from "react-router-dom";
 import resimage from "../images/restaurantfood.jpg";
 const About = () => {
-    return (
-        <div className="flex flex-col min-h-screen">
+    
 
-            <div className="flex flex-col flex-grow px-5 items-center mt-2">
+    return (
+        <div className="flex flex-col h-full min-h-screen">
+
+            <div className="flex flex-col flex-grow px-5 justify-center items-center mt-2 ">
                 <div className="flex gap-8">
-                    <img className="w-4/12 border-8 border-orange-500 rounded-[50%] hidden sm:block" src={resimage} alt="food image" />
+                    <img className="w-4/12 border-8 border-orange-500 rounded-[50%] hidden sm:block md:hidden xl:block" src={resimage} alt="food image" />
                     <div className="flex flex-col gap-5 justify-center">
                         <h1 className="text-6xl font-Dancing font-bold text-orange-500">Anytime, Anywhere..</h1>
-                        <h1 className=" font-Montserrat">Welcome to Cravings, where our journey began with a passion for revolutionizing the way people experience food. Established in 2002,
-                            we set out with a clear mission: to bring convenience, quality, and satisfaction to every doorstep. Our commitment to excellence is
+                        <p className=" text-[14px] sm:text-lg font-Montserrat">Welcome to 𝐂𝐫𝐚𝐯𝐢𝐧𝐠𝐬, where our journey began with a passion for revolutionizing the way people experience food. Established in 𝟐𝟎𝟎𝟐,
+                            we set out with a clear mission: to bring 𝐜𝐨𝐧𝐯𝐞𝐧𝐢𝐞𝐧𝐜𝐞, 𝐪𝐮𝐚𝐥𝐢𝐭𝐲, and 𝐬𝐚𝐭𝐢𝐬𝐟𝐚𝐜𝐭𝐢𝐨𝐧 to every doorstep. Our commitment to excellence is
                             reflected in our stringent quality and safety standards, ensuring that every meal delivered meets the highest expectations.
                             At the heart of our service is a customer-centric approach, where we prioritize your needs, preferences, and convenience in every order.
                             With a diverse range of restaurant partners, we offer a culinary journey that celebrates the flavors of local and international cuisines.
                             Powered by cutting-edge technology and innovation, our platform provides seamless ordering experiences and personalized recommendations.
-                            Beyond business, we are dedicated to social responsibility, actively supporting local communities and promoting sustainable practices.
+                        </p>
+                        <p className="text-[14px] sm:text-lg  font-Montserrat">Beyond business, we are dedicated to social responsibility, actively 𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐢𝐧𝐠 𝐥𝐨𝐜𝐚𝐥 𝐜𝐨𝐦𝐦𝐮𝐧𝐢𝐭𝐢𝐞𝐬 and 𝐩𝐫𝐨𝐦𝐨𝐭𝐢𝐧𝐠 𝐬𝐮𝐬𝐭𝐚𝐢𝐧𝐚𝐛𝐥𝐞 𝐩𝐫𝐚𝐜𝐭𝐢𝐜𝐞𝐬.
                             Our team is composed of industry experts and passionate individuals who drive our vision forward with creativity and dedication.
                             But don't just take our word for it, hear from our satisfied customers and restaurant partners who have experienced the Cravings difference firsthand.
-                            We invite you to join us on this culinary adventure and discover why we're more than just a food delivery app, we're a partner in creating memorable dining experiences and we're always here to serve you.</h1>
+                            We invite you to join us on this culinary adventure and discover why we're more than just a food delivery app, we're a partner in creating memorable dining experiences and we're always here to serve you.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -39,6 +43,7 @@ const About = () => {
 }
 
 export default About;
+
 
 
 
