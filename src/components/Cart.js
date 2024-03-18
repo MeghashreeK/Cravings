ef4aa8242a4d13ad98f998ac3df6d467e6d84595
MeghashreeK <kundermeghashree279@gmail.com>
2024-03-18 16:47:42 +0530

    add btn

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,23 +13,21 @@ const Cart = () => {
     const dispatch = useDispatch();
     const handleClearCart = () => {
         dispatch(clearCart());
-        console.log("cleared");
     }
 
 
 
     return (
-        <div className="flex flex-col justify-center px-2 sm:px-0 items-center mt-5 ">
-            <div className="flex justify-center w-full sm:w-6/12 ">
+        <div className="flex flex-col justify-center  items-center mt-5 min-h-screen ">
+            <div className="flex justify-center w-full px-2 sm:px-0 sm:w-6/12 flex-grow">
                 {cartItems.length === 0 ? (
                     <div className="flex flex-col justify-center items-center gap-5">
                         <img className="sm:w-6/12" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/2xempty_cart_yfxml0" alt="Cart Empty" />
-                        <div className="flex border-2 border-black"><p className="font-Montserrat italic text-black">Looks like your cart is on a diet! Let's feed it with some delicious delights!</p></div>
+                        <div className="flex text-center"><p className="font-Montserrat italic text-black">Looks like your cart is on a diet! Let's feed it with some delicious delights!</p></div>
                         <Link to="/restaurants"><button className="border-2 font-Montserrat border-orange-400 text-orange-600 sm:h-8 w-70 px-10 rounded-lg justify-center items-center hover:bg-orange-600 hover:text-white" >Add Your Favorite Food!</button></Link>
                     </div>) : (
                     <div>
-                        <ItemList items={cartItems} showAddButton={false} cartButton={true} />
-                        {/* <button className="border-4" onClick={handleClearCart}>Clear Cart</button> */}
+                        <ItemList items={cartItems}/>
                         <div className="flex flex-col gap-3 px-4">
                             <h1 className="">Bill Details:</h1>
                             <div className="flex justify-between">
@@ -72,10 +70,15 @@ const Cart = () => {
                     </div>
                 )}
             </div>
+            <div className="flex bg-black h-32 justify-center items-center sm:h-16 w-full">
+                <div className="flex gap-5 sm:gap-8">
+                    <Link to="https://www.linkedin.com/in/meghashree-kunder-017166288/"><img className="w-8 h-8" src={linkedin} /></Link>
+                    <Link to="https://github.com/MeghashreeK"><img className="w-8 h-8" src={github} /></Link>
+                </div>
+            </div>
         </div>
     );
 }
 export default Cart;
-
 
 
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,22 +6,51 @@ import veg from "../images/veg.png";
 import nonveg from "../images/nonveg.png";
 
 
-const ItemList = ({ items, showAddButton = true, cartButton = false}) => {
+const ItemList = ({ items}) => {
     const dispatch = useDispatch();
     const handleAddItem = (item) => {
         dispatch(addItem(item));
     }
-    const [quantity, setQuantity] = useState([1]);
-    const [price, setPrice] = useState([]);
+    const [quantity, setQuantity] = useState({});
+    const [cartButton,setCartButton]=useState({});
 
-    const addQuantity = () => {
-        setQuantity(parseInt(quantity) + 1);
+        useEffect(() => {
+        const initQuantity = {};
+        const initCartButton = {};
+        items.forEach(item => {
+            initQuantity[item.card.info.id] = 1;
+            initCartButton[item.card.info.id] = false;
+        });
+        setQuantity(initQuantity);
+        setCartButton(initCartButton);
+    }, [items]);
+
+    const addQuantity = (itemid) => {
+        setQuantity(prevState=>{
+            const newState={...prevState};
+            newState[itemid]=prevState[itemid]+1;
+            return newState;
+        })
     }
 
-    const subQuantity = () => {
-        if (quantity > 0) {
-            setQuantity(parseInt(quantity) - 1);
+    const subQuantity = (itemid) => {
+        if(quantity[itemid]>1){
+        setQuantity(prevState=>{
+            const newState={...prevState};
+            newState[itemid]=prevState[itemid]-1;
+            return newState;
+        })}
+        else if(quantity[itemid]==1){
+            setCartButton(false);
         }
+    }
+
+    const handleCartButton=(itemid)=>{
+        setCartButton(prevState=>{
+        const newState={...prevState};
+        newState[itemid]=true;
+        return newState;
+        });
     }
 
     return (
@@ -31,13 +60,22 @@ const ItemList = ({ items, showAddButton = true, cartButton = false}) => {
                     <div className="flex flex-col items-start w-9/12 gap-1">
                     <span>{item.card.info.itemAttribute.vegClassifier==="VEG" ? <img className="h-5 w-5" src={veg} alt="veg"/> : <img className="h-5 w-5" src={nonveg} alt="veg"/> }</span>
                         <span>{item.card.info.name}</span>
-                        <span> ₹{((item.card.info.price || item.card.info.defaultPrice) / 100) * quantity}</span>
+                        <span> ₹{((item.card.info.price || item.card.info.defaultPrice) / 100) * quantity[item.card.info.id]}</span>
+                        
+
                         <p className="text-gray-400 text-left mb-2">{item.card.info.description}</p>
                     </div>
                     <div className="w-3/12 flex flex-col items-center" >
                         <img className="object-cover h-28 w-36 rounded-lg" src={CDN_URL + item.card.info.imageId} />
-                        {showAddButton && <button className="border w-12 rounded-lg bg-orange-400 text-xs " onClick={() => handleAddItem(item)}>Add+</button>}
-                        {cartButton && <button><button onClick={subQuantity}>-</button>{quantity}<button onClick={addQuantity}>+</button></button>}
+                        {!cartButton[item.card.info.id] && (<button className="border px-3 sm:px-5 rounded-lg bg-orange-400 " onClick={() => {handleAddItem(item); handleCartButton(item.card.info.id);}}>Add+</button>)}
+                        {
+                        cartButton[item.card.info.id] &&
+                        (<div className="border border-orange-400 px-2 rounded-lg">
+                            <button className="px-5" onClick={()=>subQuantity(item.card.info.id)}>-</button>
+                            {quantity[item.card.info.id]}
+                            <button className="px-5" onClick={()=>addQuantity(item.card.info.id)}>+</button>
+                        </div>)
+                        }
                     </div>
                 </div>
             </div>)}
@@ -47,3 +85,106 @@ const ItemList = ({ items, showAddButton = true, cartButton = false}) => {
 }
 export default ItemList;
 
+
+
+
+// import { useState, useEffect } from "react";
+// import { addItem } from "../utils/cartSlice";
+// import { CDN_URL } from "../utils/constants";
+// import { useDispatch } from "react-redux";
+// import veg from "../images/veg.png";
+// import nonveg from "../images/nonveg.png";
+
+// const ItemList = ({ items, showAddButton = true }) => {
+//     const dispatch = useDispatch();
+
+//     const [quantities, setQuantities] = useState({});
+//     const [cartButtons, setCartButtons] = useState({});
+
+//     useEffect(() => {
+//         const initQuantity = {};
+//         const initCartButton = {};
+//         items.forEach(item => {
+//             initQuantity[item.card.info.id] = 1;
+//             initCartButton[item.card.info.id] = false;
+//         });
+//         setQuantities(initQuantity);
+//         setCartButtons(initCartButton);
+//     }, [items]);
+
+//     const handleAddItem = (item) => {
+//         dispatch(addItem(item));
+//     }
+
+//     const addQuantity = (itemId) => {
+//         setQuantities(prevState => {
+//             const newState = { ...prevState };
+//             newState[itemId] = prevState[itemId] + 1;
+//             return newState;
+//         });
+//     }
+
+//     const subQuantity = (itemId) => {
+//         if (quantities[itemId] > 0) {
+
+//             setQuantities(prevState => {
+//                 const newState = { ...prevState };
+//                 newState[itemId] = prevState[itemId] - 1;
+//                 return newState;
+//             });
+//         }
+//         else if(quantities[itemId]<1){
+//             showAddButton=true;
+//             setCartButtons(false);  
+//         }
+        
+//     }
+    
+    
+
+//     const handleCartButton = (itemId) => {
+//         setCartButtons(prevState => {
+//             const newState = { ...prevState };
+//             newState[itemId] = true;
+//             return newState;
+//         });
+//     }
+// console.log(quantities);
+//     return (
+//         <div>
+
+//             {items.map((item, index) => (
+//                 <div data-testid="foodItems" key={item.card.info.id}>
+
+//                     <div className="flex gap-10 pl-4 pr-4 mb-8 border-b-2">
+
+//                         <div className="flex flex-col items-start w-9/12 gap-1">
+
+//                             <span>{item.card.info.itemAttribute.vegClassifier === "VEG" ? <img className="h-5 w-5" src={veg} alt="veg" /> : <img className="h-5 w-5" src={nonveg} alt="veg" />}</span>
+//                             <span>{item.card.info.name}</span>
+//                             <span> ₹{((item.card.info.price || item.card.info.defaultPrice) / 100) * quantities[item.card.info.id]}</span>
+//                             <p className="text-gray-400 text-left mb-2">{item.card.info.description}</p>
+//                         </div>
+//                         <div className="w-3/12 flex flex-col items-center" >
+//                             <img className="object-cover h-28 w-36 rounded-lg" src={CDN_URL + item.card.info.imageId} />
+//                             {showAddButton && !cartButtons[item.card.info.id] && (
+//                                 <button className="border w-12 rounded-lg bg-orange-400 text-xs" onClick={() => { handleAddItem(item); handleCartButton(item.card.info.id); }}>Add+</button>
+//                             )}
+//                             {cartButtons[item.card.info.id] && (
+//                                 <div>
+//                                     <button onClick={() => subQuantity(item.card.info.id)}>-</button>
+//                                     {quantities[item.card.info.id]}
+//                                     <button onClick={() => addQuantity(item.card.info.id)}>+</button>
+//                                 </div>
+//                             )}
+
+//                         </div>
+//                     </div>
+
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
+
+// export default ItemList;
