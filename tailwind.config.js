098e5fd22398ed4825a0f7a7c19ca5d85c1817aa
MeghashreeK <kundermeghashree279@gmail.com>
2024-04-19 12:02:21 +0530

    shimmerB responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{html,js}",],
+  content: ["./src/**/*.{html,js}",],
   theme: {
     extend: {
-      fontFamily:{
-        Dancing:['Dancing Script'],
-        Montserrat:['Montserrat']
+      fontFamily: {
+        Dancing: ['Dancing Script'],
+        Montserrat: ['Montserrat']
       }
     },
     screens: {
+      'xsm': '200px',
 
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
@@ -19,17 +20,20 @@ module.exports = {
       'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
+      'lg1': '1226px',
+      // => @media (min-width: 1024px) { ... }
+
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
 
-      'xl1':'1345px',
+      'xl1': '1345px',
 
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
     }
   },
   plugins: [],
-  
+
 }
 
 
